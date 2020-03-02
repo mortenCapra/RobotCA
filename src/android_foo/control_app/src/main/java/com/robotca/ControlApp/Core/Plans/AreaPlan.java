41a12525c5869d1470e72e1e1855b9c38fe9537b
mortenCapra <morten@capra.ooo>
2020-03-02 13:43:13 +0100

    Added new way to move robot in area, deleted old way.

--- src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/Plans/AreaPlan.java
+++ src/android_foo/control_app/src/main/java/com/robotca/ControlApp/Core/Plans/AreaPlan.java
@@ -13,19 +13,12 @@ import org.osmdroid.util.GeoPoint;
 import java.util.ArrayList;
 import java.util.Random;
 
-import geometry_msgs.Pose;
-
 public class AreaPlan extends RobotPlan {
 
     private static final String TAG = "AreaPlan";
 
     private final Random random;
-    private ArrayList<Double> results;
     private ControlApp controlApp;
-
-    ArrayList<Double> a;
-    ArrayList<Double> b;
-    ArrayList<Double> distance;
 
     /**
      * Creates an AreaPlan with the specified area.
@@ -33,10 +26,6 @@ public class AreaPlan extends RobotPlan {
     public AreaPlan(ControlApp controlApp) {
         this.controlApp = controlApp;
         random = new Random();
-        results = new ArrayList<>();
-        a = new ArrayList<>();
-        b = new ArrayList<>();
-        distance = new ArrayList<>();
     }
 
     /**
@@ -52,9 +41,11 @@ public class AreaPlan extends RobotPlan {
 
         Log.d(TAG, "Started");
 
+        double result;
         ArrayList<GeoPoint> areaPoints;
-
-        double robotLatX, robotLonY;
+        ArrayList<Double> distance = new ArrayList<>();
+        ArrayList<Double> distanceBetweenPoints = new ArrayList<>();
+        ArrayList<Double> distanceBetweenPointAndRobot = new ArrayList<>();
         Location location;
 
         while (!isInterrupted()) {
@@ -70,144 +61,59 @@ public class AreaPlan extends RobotPlan {
             areaPoints = controlApp.getAreaPoints();
 
             for (int i = 0; i < areaPoints.size() - 1; i++) {
-                double aTemp = ((areaPoints.get(i + 1).getLongitude() - areaPoints.get(i).getLongitude()) / (areaPoints.get(i + 1).getLatitude() - areaPoints.get(i).getLatitude()));
-                double bTemp = areaPoints.get(i).getLongitude() - aTemp * areaPoints.get(i).getLatitude();
+                double startLon = areaPoints.get(i).getLongitude();
+                double startLat = areaPoints.get(i).getLatitude();
+                double endLon = areaPoints.get(i+1).getLongitude();
+                double endLat = areaPoints.get(i+1).getLatitude();
 
-                a.add(aTemp);
-                b.add(bTemp);
+                double dPoints = MapFragment.computeDistanceToKilometers(startLat, startLon, endLat, endLon);
+                double dRobot = MapFragment.computeDistanceToKilometers(startLat, startLon, location.getLatitude(), location.getLongitude());
+
+                distanceBetweenPoints.add(dPoints);
+                distanceBetweenPointAndRobot.add(dRobot);
+                if (i == areaPoints.size() - 2) {
+                    distanceBetweenPointAndRobot.add(distanceBetweenPointAndRobot.get(0));
+                }
             }
 
-            for (int i = 0; i < a.size() - 1; i++) {
-                double y2 = areaPoints.get(i + 2).getLongitude();
-                double y1 = areaPoints.get(i).getLongitude();
-                double y0 = location.getLongitude();
-                double x2 = areaPoints.get(i + 2).getLatitude();
-                double x1 = areaPoints.get(i).getLatitude();
-                double x0 = location.getLatitude();
-
-                double d = (Math.abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) / Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2)));
+            for (int i = 0; i < areaPoints.size() - 1; i++) {
+                double perimeter = (distanceBetweenPoints.get(i) + distanceBetweenPointAndRobot.get(i) + distanceBetweenPointAndRobot.get(i+1)) / 2;
+                double area = Math.sqrt(perimeter * (perimeter - distanceBetweenPoints.get(i)) * (perimeter - distanceBetweenPointAndRobot.get(i)) * (perimeter - distanceBetweenPointAndRobot.get(i+1)));
+                double d = ((2 * area) / distanceBetweenPoints.get(i)) * 100000;
 
                 distance.add(d);
             }
 
-            for (double d: distance) {
-                if (d < 1) {
-                    controller.publishVelocity(0, 0, 0);
-                } else {
-                    controller.publishVelocity(1, 0, 0);
+            result = distance.get(0);
+
+            for (int i = 0; i < distance.size(); i++) {
+                if (result > distance.get(i)) {
+                    result = distance.get(i);
                 }
             }
 
-            a.clear();
-            b.clear();
+            if (result < 150) {
+                controller.publishVelocity(0, 0, 0);
+                waitFor(1000);
+                controller.publishVelocity(-1, 0, 0);
+                waitFor(2000);
+                controller.publishVelocity(0, 0, 0);
+                waitFor(1000);
+                long delay = (long) (2000 * (1 + random.nextFloat()));
+                controller.publishVelocity(0, 0, .75);
+                waitFor(delay);
+                controller.publishVelocity(0, 0, 0);
+                waitFor(1000);
+            } else {
+                controller.publishVelocity(1, 0, 0);
+                // For debugging
+                waitFor(1000);
+                controller.publishVelocity(0, 0, 0);
+            }
+
             distance.clear();
-
-            /*double highestLonY = areaPoints.get(0).getLongitude();
-            double highestLatX = areaPoints.get(0).getLatitude();
-            double lowestLonY = areaPoints.get(0).getLongitude();
-            double lowestLatX = areaPoints.get(0).getLatitude();
-
-            for (int i = 0; i < areaPoints.size() - 1; i++) {
-                if (highestLonY < areaPoints.get(i).getLongitude()) {
-                    highestLonY = areaPoints.get(i).getLongitude();
-                }
-                if (highestLatX < areaPoints.get(i).getLatitude()) {
-                    highestLatX = areaPoints.get(i).getLatitude();
-                }
-                if (lowestLonY > areaPoints.get(i).getLongitude()) {
-                    lowestLonY = areaPoints.get(i).getLongitude();
-                }
-                if (lowestLatX > areaPoints.get(i).getLatitude()) {
-                    lowestLatX = areaPoints.get(i).getLatitude();
-                }
-            }
-
-            if (location.getLongitude() >= lowestLonY && location.getLatitude() >= lowestLatX
-                    && location.getLongitude() <= highestLonY && location.getLatitude() <= highestLatX) {
-                controller.publishVelocity(1, 0, 0);
-            } else if (location.getLongitude() <= lowestLonY || location.getLatitude() <= lowestLatX
-                    || location.getLongitude() >= highestLonY || location.getLatitude() >= highestLatX) {
-                controller.publishVelocity(-1, 0, 0);
-                waitFor(1000);
-                long delay = (long) (2000 * (1 + random.nextFloat()));
-                controller.publishVelocity(0, 0, .75);
-                waitFor(delay);
-                controller.publishVelocity(0, 0, 0);
-                waitFor(1000);
-                controller.publishVelocity(1, 0, 0);
-                waitFor(1000);
-                controller.publishVelocity(0, 0, 0);
-                waitFor(1000);
-            }*/
-
-            /*robotLatX = location.getLatitude();
-            robotLonY = location.getLongitude();
-
-            // Calculate distance from robot to all points
-            for (int i = 0; i < areaPoints.size() - 1; i++) {
-                double latX = areaPoints.get(i).getLatitude();
-                double lonY = areaPoints.get(i).getLongitude();
-
-                double distance = MapFragment.computeDistanceToKilometers(latX, lonY, robotLatX, robotLonY);
-
-                results.add(distance);
-            }
-
-            double minValue = results.get(0);
-
-            // Calculate closest point to robot
-            for (int i = 0; i < results.size() - 1; i++) {
-                if (minValue > results.get(i)) {
-                    minValue = results.get(i);
-                }
-            }
-
-            // Get closest point, previous point and next point to closest point
-            GeoPoint first = areaPoints.get(results.indexOf(minValue));
-            GeoPoint second;
-            if (first == areaPoints.get(0)) {
-                second = areaPoints.get(areaPoints.size() - 2);
-            } else {
-                second = areaPoints.get(results.indexOf(minValue) - 1);
-            }
-            GeoPoint third = areaPoints.get(results.indexOf(minValue) + 1);
-
-            results.clear();
-
-            // Calculate distance from closest point to previous point and next point
-            double distanceSecond = MapFragment.computeDistanceToKilometers(first.getLatitude(), first.getLongitude(), second.getLatitude(), second.getLongitude());
-            double distanceThird = MapFragment.computeDistanceToKilometers(first.getLatitude(), first.getLongitude(), third.getLatitude(), third.getLongitude());
-            double A;
-
-            // Calculate angle between robot and the two points
-            if (distanceSecond < distanceThird || distanceSecond == distanceThird) {
-                double distBetweenSecondAndRobot = MapFragment.computeDistanceToKilometers(robotLatX, robotLonY, second.getLatitude(), second.getLongitude());
-                A = Math.acos((Math.pow(minValue, 2) + Math.pow(distBetweenSecondAndRobot, 2) - Math.pow(distanceSecond, 2)) / (2 * minValue * distBetweenSecondAndRobot));
-            } else if (distanceSecond > distanceThird) {
-                double distBetweenThirdAndRobot = MapFragment.computeDistanceToKilometers(robotLatX, robotLonY, third.getLatitude(), third.getLongitude());
-                A = Math.acos((Math.pow(minValue, 2) + Math.pow(distBetweenThirdAndRobot, 2) - Math.pow(distanceThird, 2)) / (2 * minValue * distBetweenThirdAndRobot));
-            } else {
-                A = 0;
-            }
-
-            A = A * 180 / Math.PI;*/
-
-            // If angle gets to big, change direction
-            /*if (A > 160) {
-                controller.publishVelocity(0, 0, 0);
-                waitFor(1000);
-                controller.publishVelocity(-1, 0, 0);
-                waitFor(1000);
-                long delay = (long) (2000 * (1 + random.nextFloat()));
-                controller.publishVelocity(0, 0, .75);
-                waitFor(delay);
-                controller.publishVelocity(0, 0, 0);
-                waitFor(1000);
-            } else {
-                controller.publishVelocity(1, 0, 0);
-                waitFor(1000);
-                controller.publishVelocity(0, 0, 0);
-            }*/
+            distanceBetweenPoints.clear();
+            distanceBetweenPointAndRobot.clear();
         }
     }
 }
